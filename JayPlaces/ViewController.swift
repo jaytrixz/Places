9c85ad3ee0a0b32ed7bea56ef139c621cb4459c8
John Joseph Santos <john_joseph.santos@bell.ca>
2020-10-26 01:30:18 +0800

    Initial Commit

---
+++ JayPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JayPlaces
+//
+//  Created by John Joseph M. Santos on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
